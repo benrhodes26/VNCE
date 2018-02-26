f5216db41b83108a968c640a35fb08e47f6c6829
Ben <s1771906@ed.ac.uk>
2018-02-26 22:21:04 +0000

    Add standard NCE optimiser for comparison with latent variable optimiser

--- proposal/code/latent_nce_optimiser.py
+++ proposal/code/latent_nce_optimiser.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 from scipy.optimize import minimize
 
 
-# noinspection PyPep8Naming,PyTypeChecker
+# noinspection PyPep8Naming,PyTypeChecker,PyMethodMayBeStatic
 class LatentNCEOptimiser:
     """ Optimiser for estimating/learning the parameters of an unnormalised
     model with latent variables. This Optimiser estimates all expectations
@@ -75,7 +75,7 @@ class LatentNCEOptimiser:
     def compute_J1(self, X, ZX, ZY):
         """Return MC estimate of Lower bound of NCE objective
 
-        :param x: array (N, d)
+        :param X: array (N, d)
             data sample we are training our model on
         :param ZX: (N, nz, m)
             latent variable samples for data x. for each
@@ -220,6 +220,8 @@ class LatentNCEOptimiser:
             list of function evaluations of J1 during optimisation
         :param J1_grads: list
             list of function evaluations of J1_grad during optimisation
+        :param disp: bool
+            display optimisation results for each iteration
         :return new_theta: array
             new theta that maximises the J1 defined by input theta
         """
@@ -237,14 +239,11 @@ class LatentNCEOptimiser:
             J1_grad_vals.append(-grad_val)
             return grad_val
 
-        res = minimize(J1_k_neg, self.phi.theta, method='BFGS', jac=J1_k_grad_neg,
+        _ = minimize(J1_k_neg, self.phi.theta, method='BFGS', jac=J1_k_grad_neg,
                        options={'gtol': 1e-5, 'disp': disp})
 
         J1s.append(J1_vals)
         J1_grads.append(J1_grad_vals)
-        new_theta = res.x
-
-        return new_theta
 
     def plot_loss_curve(self, J1s):
         num_fevals_per_step = [len(i) for i in J1s]
@@ -264,7 +263,7 @@ class LatentNCEOptimiser:
         return "LatentNCEOptimiser"
 
 
-# noinspection PyPep8Naming,PyTypeChecker
+# noinspection PyPep8Naming,PyTypeChecker,PyMethodMayBeStatic
 class LatentNCEOptimiserWithAnalyticExpectations:
     """ Optimiser for estimating/learning the parameters of an unnormalised
     model with latent variables. This Optimiser estimates all expectations
@@ -275,9 +274,9 @@ class LatentNCEOptimiserWithAnalyticExpectations:
 
     1) E(r(u, z))
     2) E(log(psi_1(u,z)))
-    3) E( grad_theta(log(phi(u,z)) (psi_1(u, z) - 1) / psi_1(u, z))
-    4) E( grad_theta(log(phi(u,z)) r(u, z) )
-    5) grad_alpha (E(log(1 + nu/r(u, z))))
+    3) E(grad_theta(log(phi(u,z)) (psi_1(u, z) - 1) / psi_1(u, z))
+    4) E(grad_theta(log(phi(u,z)) r(u, z) )
+    5) grad_alpha(E(log(1 + nu/r(u, z))))
 
     Where each expectation is over z ~ q(z | u; alpha).
     r and psi_1 are given by:
@@ -341,7 +340,7 @@ class LatentNCEOptimiserWithAnalyticExpectations:
     def compute_J1(self, X):
         """Return analytic expression of Lower bound of NCE objective
 
-        :param x: array (N, d)
+        :param X: array (N, d)
             data sample we are training our model on
         :return: float
             Value of objective for current parameters
@@ -494,14 +493,11 @@ class LatentNCEOptimiserWithAnalyticExpectations:
             J1_grad_vals.append(-grad_val)
             return grad_val
 
-        res = minimize(J1_k_neg, self.phi.theta, method='BFGS', jac=J1_k_grad_neg,
+        _ = minimize(J1_k_neg, self.phi.theta, method='BFGS', jac=J1_k_grad_neg,
                        options={'gtol': 1e-5, 'disp': disp})
 
         J1s.append(J1_vals)
         J1_grads.append(J1_grad_vals)
-        new_theta = res.x
-
-        return new_theta
 
     def maximize_J1_wrt_alpha(self, X, J1s, J1_grads, disp):
         """Return alpha that maximises J1
@@ -531,14 +527,11 @@ class LatentNCEOptimiserWithAnalyticExpectations:
             J1_grad_vals.append(-grad_val)
             return grad_val
 
-        res = minimize(J1_k_neg, self.q.alpha, method='BFGS', jac=J1_k_grad_neg,
-                       options={'gtol': 1e-5, 'disp': disp})
+        _ = minimize(J1_k_neg, self.q.alpha, method='BFGS', jac=J1_k_grad_neg,
+                     options={'gtol': 1e-5, 'disp': disp})
 
         J1s.append(J1_vals)
         J1_grads.append(J1_grad_vals)
-        new_alpha = res.x
-
-        return new_alpha
 
     def plot_loss_curve(self, J1s):
         num_fevals_per_step = [len(i) for i in J1s]
@@ -552,6 +545,169 @@ class LatentNCEOptimiserWithAnalyticExpectations:
                 axs.plot(cum_fevals[i] * np.array([1, 1]), plt.get(axs, 'ylim'), 'r--')
             else:
                 axs.plot(cum_fevals[i] * np.array([1, 1]), plt.get(axs, 'ylim'), 'b--')
+
+    def __repr__(self):
+        return "LatentNCEOptimiser"
+
+
+# noinspection PyMethodMayBeStatic,PyPep8Naming
+class NCEOptimiser:
+    """ Optimiser for estimating/learning the parameters of an unnormalised
+    model as proposed in http://proceedings.mlr.press/v9/gutmann10a/gutmann10a.pdf
+
+    This class wraps together the NCE objective function, its gradients with respect
+    to the parameters of the unnormalised model and a method for performing the
+    optimisation: self.fit().
+
+    """
+    def __init__(self, model, noise, sample_size, nu=1, eps=1e-15):
+        """ Initialise unnormalised model and noise distribution
+
+        :param model: LatentVariableModel
+            unnormalised model whose parameters theta we want to optimise.
+            Has arguments (U, theta) where:
+            - U is (n, d) array of data
+        :param noise: Distribution
+            noise distribution required for NCE. Has argument U where:
+            - U is (n*nu, d) array of noise
+        :param nu: ratio of noise to model samples
+        :param eps: small constant needed to avoid division by zero
+        """
+        self.phi = model
+        self.pn = noise
+        self.nu = nu
+        self.sample_size = sample_size
+        self.eps = eps
+        self.Y = self.pn.sample(int(sample_size * nu))  # generate noise
+
+    def h(self, U):
+        """ Compute log(phi(U)) - log(pn(U).
+        :param U: array of shape (?, d)
+            U can be either data or noise samples, so ? is either n or n*nu
+        :return: array of shape (?)
+            ? is either n or n*nu
+        """
+        return np.log(self.phi(U)) - np.log(self.pn(U))
+
+    def compute_J1(self, X):
+        """Return MC estimate of Lower bound of NCE objective
+
+        :param X: array (N, d)
+            data sample we are training our model on
+        :return: float
+            Value of objective for current parameters
+        """
+        Y, nu = self.Y, self.nu
+
+        a0 = 1 + nu*np.exp(-self.h(X))
+        a1 = 1 + (1 / nu)*np.exp(self.h(Y))
+
+        first_term = - np.mean(np.log(a0))
+        second_term = - nu*np.mean(np.log(a1))
+
+        return first_term + second_term
+
+    def compute_J1_grad(self, X):
+        """Computes J1_grad w.r.t theta using Monte-Carlo
+
+        :param X: array (N, d)
+            data sample we are training our model on.
+        :return grad: array of shape (len(phi.theta), )
+        """
+        Y, nu = self.Y, self.nu
+
+        gradX = self.phi.grad_log_wrt_params(X)  # (len(theta), n)
+        # a0 = 1 / (1 + (1 / nu)*np.exp(self.h(X)))  # (n,)
+        a0 = nu*self.pn(X) / (nu*self.pn(X) + self.phi(X))  # (n,)
+        # expectation over X
+        term_1 = np.mean(gradX*a0, axis=1)  # (len(theta), )
+
+        gradY = self.phi.grad_log_wrt_params(Y)  # (len(theta), nu*n)
+        a1 = self.phi(Y) / (nu*self.pn(Y) + self.phi(Y))  # (n)
+        # Expectation over Y
+        term_2 = - nu * np.mean(gradY*a1, axis=1)  # (len(theta), )
+
+        grad = term_1 + term_2
+
+        # If theta is 1-dimensional, grad will be a float.
+        if isinstance(grad, float):
+            grad = np.array(grad)
+        assert grad.shape == self.phi.theta_shape, ' ' \
+            'Expected grad to be shape {}, got {} instead'.format(self.phi.theta_shape,
+                                                                  grad.shape)
+        return grad
+
+    def fit(self, X, theta0=np.array([0.5]), disp=True, plot=True):
+        """ Fit the parameters of the model to the data X by optimising
+        the objective function defined in self.compute_J1(). To do this,
+        we use a gradient-based optimiser and define the gradient of J1
+        with respect to its parameters theta in self.compute_J1_grad().
+
+        :param X: Array of shape (num_data, data_dim)
+            data that we want to fit the model to
+        :param theta0: array of shape (1, )
+            initial value of theta used for optimisation
+        :param disp: bool
+            display output from scipy.minimize
+        :param plot: bool
+            plot optimisation loss curve
+        :return
+            thetas_after_em_step: (?, num_em_steps),
+            J1s: list of lists
+                each inner list contains the fevals computed for a
+                single step (loop) in the EM-type optimisation
+            J1_grads: list of lists
+                each inner list contains the grad fevals computed for a
+                single step (loop) in the EM-type optimisation
+        """
+        # initialise parameters
+        self.phi.theta = theta0
+
+        # optimise w.r.t to theta
+        J1s, J1_grads = self.maximize_J1_wrt_theta(X, disp)
+
+        if plot:
+            J1s = self.plot_loss_curve(J1s)
+
+        return np.array(J1s), np.array(J1_grads)
+
+    def maximize_J1_wrt_theta(self, X, disp):
+        """Return theta that maximises J1
+
+        :param X: array (n, 1)
+            data
+        :param disp: bool
+            display optimisation results for each iteration
+        :return J1s: list
+            function evaluations during optimisation
+                J1_grads: list
+            gradient evaluations during optimisation
+        """
+        J1s, J1_grads = [], []
+
+        def J1_k_neg(theta):
+            self.phi.theta = theta
+            val = -self.compute_J1(X)
+            J1s.append(-val)
+            return val
+
+        def J1_k_grad_neg(theta):
+            self.phi.theta = theta
+            grad_val = -self.compute_J1_grad(X)
+            J1_grads.append(-grad_val)
+            return grad_val
+
+        _ = minimize(J1_k_neg, self.phi.theta, method='BFGS', jac=J1_k_grad_neg,
+                       options={'gtol': 1e-5, 'disp': disp})
+
+        return J1s, J1_grads
+
+    def plot_loss_curve(self, J1s):
+        fig, axs = plt.subplots(1, 1, figsize=(10, 7))
+        t = np.arange(len(J1s))
+        axs.plot(t, J1s, c='k')
+
+        return J1s
 
     def __repr__(self):
         return "LatentNCEOptimiser"
--- proposal/code/latent_variable_model.py
+++ proposal/code/latent_variable_model.py
@@ -54,8 +54,10 @@ class LatentVarModel(metaclass=ABCMeta):
     def grad_log_wrt_params(self, U, Z):
         """Returns nabla_theta(log(phi(x, theta)))
 
-        :param X: array (N, d)
+        :param U: array (n, d)
             data sample we are training our model on
+        :param Z: (nz, n, m)
+            nz*n 1-dimensional latent variable samples for data U.
         """
         raise NotImplementedError
 
@@ -63,13 +65,13 @@ class LatentVarModel(metaclass=ABCMeta):
 # noinspection PyPep8Naming,PyMissingConstructor
 class MixtureOfTwoGaussians(LatentVarModel):
     """ Mixture of two Gaussians given by :
-    phi(u; theta) = (1/2)*N(u; 0, sigma0) + (1/2)*N(u; 0, sigma1)
+    phi(u; theta) = (1/2)*Z*N(u; 0, sigma0) + (1/2)*(1-Z)N(u; 0, sigma1)
+    where Z is a binary latent variable
     """
     def __init__(self, theta, sigma1=1):
         """Initialise std deviations of gaussians
 
         :param theta: array of shape (1, )
-        :param sigma0: float
         :param sigma1: float
         """
         self.sigma1 = sigma1
@@ -248,7 +250,7 @@ class MixtureOfTwoUnnormalisedGaussians(LatentVarModel):
         """
         nz, n = Z.shape[0], Z.shape[1]
         Z = Z.reshape(nz, n)
-        grad = np.zeros(len(self.theta), nz, n)
+        grad = np.zeros((len(self.theta), nz, n))
 
         grad[0] = -1  # grad w.r.t scaling param
         a = (U**2/self.theta[1]**3)  # (n, 1)
@@ -273,7 +275,8 @@ class MixtureOfTwoUnnormalisedGaussians(LatentVarModel):
         return grad  # (2, n)
 
     def sample(self, n):
-        """ Sample n values from the model
+        """ Sample n values from the model, with uniform(0,1) dist over latent vars
+
         :param n: number of data points to sample
         :return: data array of shape (n, 1)
         """
@@ -313,6 +316,155 @@ class MixtureOfTwoUnnormalisedGaussians(LatentVarModel):
         plt.plot(u, x_density_samples, label='kde')
 
         px = self.normalised_and_marginalised_over_z(u)
+        plt.plot(u, px, label='true', c='r', linestyle='--')
+
+        plt.legend()
+        plt.grid()
+
+
+# noinspection PyPep8Naming
+class Model(metaclass=ABCMeta):
+
+    def __init__(self, theta):
+        """ Initialise parameters of model: theta.
+
+        Theta should be a one-dimensional array or int/float"""
+        if isinstance(theta, float) or isinstance(theta, int):
+            theta = np.array([theta])
+        assert theta.ndim == 1, 'Theta should have dimension 1, ' \
+                                'not {}'.format(theta.ndim)
+        self._theta = theta
+        self.theta_shape = theta.shape
+
+    @property
+    def theta(self):
+        return self._theta
+
+    @theta.setter
+    def theta(self, value):
+        if isinstance(value, float) or isinstance(value, int):
+            value = np.array([value])
+        assert value.ndim == 1, 'Theta should have dimension 1, ' \
+                                'not {}'.format(value.ndim)
+        assert value.shape == self.theta_shape, 'Tried to ' \
+            'set theta to array with shape {}, should be {}'.format(value.shape,
+                                                                    self.theta_shape)
+        self._theta = value
+
+    @abstractmethod
+    def __call__(self, U):
+        """Evaluate model on data
+
+        :param U: array (n, d)
+            n input data points to evaluate model on
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def grad_log_wrt_params(self, U):
+        """Returns nabla_theta(log(phi(x, theta)))
+
+        :param U: array (n, d)
+            data sample we are training our model on
+        """
+        raise NotImplementedError
+
+
+# noinspection PyPep8Naming
+class SumOfTwoUnnormalisedGaussians(Model):
+    """Sum of two unnormalisedGaussians (no latent variable) given by :
+    phi(u; theta) = e^c*(N(u; 0, sigma0) + N(u; 0, sigma1))"""
+
+    def __init__(self, theta, sigma1=1):
+        """Initialise std deviations of gaussians
+
+        :param theta: array of shape (1, )
+        :param sigma1: float
+        """
+        self.sigma1 = sigma1
+        super().__init__(theta)
+
+    def __call__(self, U):
+        """ Evaluate model for each data point U[i, :]
+
+        :param U: array (n, 1)
+             either data or noise for NCE
+        :return array (n)
+        """
+        U = U.reshape(-1)
+        a = np.exp(-self.theta[0])  # scaling parameter
+        term_1 = np.exp(-U**2 / (2 * self.theta[1]**2))
+        term_2 = np.exp(-U**2 / (2 * self.sigma1**2))
+
+        return a*(term_1 + term_2)
+
+    def grad_log_wrt_params(self, U):
+        """ Nabla_theta(log(phi(x,z; theta))) where phi is the unnormalised model
+
+        :param U: array (n, 1)
+             either data or noise for NCE
+        :return grad: array (len(theta)=2, nz, n)
+        """
+        n = U.shape[0]
+        grad = np.zeros((len(self.theta), n))
+
+        grad[0] = -1  # grad w.r.t scaling param
+
+        a = np.exp(-U**2 / (2 * self.theta[1] ** 2))
+        b = np.exp(-U**2 / (2 * self.sigma1 ** 2))
+        c = a / (a + b)  # (n, 1)
+
+        d = (U**2 / self.theta[1]**3)  # (n, 1)
+        grad[1] = (c*d).reshape(-1)  # (n, )
+
+        correct_shape = self.theta_shape + (n, )
+        assert grad.shape == correct_shape, ' ' \
+                                            'gradient should have shape {}, got {} instead'.format(correct_shape,
+                                                                                                   grad.shape)
+        return grad
+
+    def sample(self, n):
+        """ Sample n values from the model, with uniform(0,1) dist over latent vars
+
+        :param n: number of data points to sample
+        :return: data array of shape (n, 1)
+        """
+        a = self.sigma1 / (self.theta[1] + self.sigma1)
+        w = rnd.uniform(0, 1, n) < a
+        x = (w == 0)*(rnd.randn(n)*self.theta[1]) + (w == 1)*(rnd.randn(n)*self.sigma1)
+        return x.reshape(-1, 1)
+
+    def normalised(self, U):
+        """Return values of p(U), where p is the normalised distribution over x
+
+        :param U: array (n, 1)
+             either data or noise for NCE
+        :return array (n, )
+            probabilities of datapoints under p(x) i.e with z marginalised out
+            and the distribution has been normalised
+        """
+        a = self.theta[1] / (self.theta[1] + self.sigma1)
+        return a*norm.pdf(U.reshape(-1), 0, self.theta[1]) + (1 - a)*norm.pdf(U.reshape(-1), 0, self.sigma1)
+
+        # noinspection PyUnusedLocal
+    def plot_sample_density_against_true_density(self, X, figsize=(10, 7), bandwidth=0.2):
+        """Compare kernel density estimate of sample X to true density
+
+        :param X: array (N, 1)
+            X is a sample, possibly generated from self.sample
+        :param figsize: tuple
+            size of figure
+        :param bandwidth:
+            bandwidth parameter passed to sklearn.KernelDensity
+        """
+        fig = plt.figure(figsize=figsize)
+        u = np.arange(-10, 10, 0.01)
+
+        x_density = kd(bandwidth=bandwidth).fit(X.reshape(-1, 1))
+        x_density_samples = np.exp(x_density.score_samples(u.reshape(-1, 1)))
+        plt.plot(u, x_density_samples, label='kde')
+
+        px = self.normalised(u)
         plt.plot(u, px, label='true', c='r', linestyle='--')
 
         plt.legend()
